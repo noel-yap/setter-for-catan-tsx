f01dc241cc6f033d7c2c64039807ea1a97b3e98a
Noel Yap <noel.yap+github.com@gmail.com>
2019-03-17 11:57:27 -0700

    oeu

--- scripts/bake.sh
+++ scripts/bake.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+version=$1
+
 docker \
     run -it \
     baker \
     /bin/bash -c '\
         cd ${HOME} && \
-        git clone https://github.com/november-yankee/setter-for-catan-tsx.git --branch v0.3.0 --single-branch && \
+        git clone https://github.com/november-yankee/setter-for-catan-tsx.git --branch "v${version}" --single-branch && \
         cd setter-for-catan-tsx && \
         scripts/build.sh && \
         scripts/package.sh && \
