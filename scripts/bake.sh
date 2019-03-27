cc8d77e1aed3f11287d239203f0c1a9125553f48
Noel Yap <noel.yap+github.com@gmail.com>
2019-03-27 15:18:39 -0700

    Bake implemented.

--- scripts/bake.sh
+++ scripts/bake.sh
@@ -1,19 +1,39 @@
 #!/bin/bash -evx
 
-version=$1
+umask 0022
+
+project_name=$(basename $(git remote get-url origin) .git)
+project_version=$1
+if [ -z "${project_version}" ]
+then
+  project_version=$(git rev-parse HEAD)
+fi
 
 image_id=02485d266f742e8b2
 security_group_ids=0252c5c7a1eeec1b3
 
 instance_ids=$(aws ec2 run-instances --image-id "ami-${image_id}" --count 1 --instance-type t2.micro --security-group-ids "sg-${security_group_ids}" | jq '.Instances[].InstanceId' | sed -e 's|"||g')
-echo "instance_ids = ${instance_ids}"
+echo "instance_ids = [${instance_ids}]"
+
+if [ -z "${instance_ids}" ]
+then
+  echo Error
+  aws ec2 run-instances --image-id "ami-${image_id}" --count 1 --instance-type t2.micro --security-group-ids "sg-${security_group_ids}"
+  exit 1
+fi
 
 $(dirname $0)/build.sh
-artifact=$($(dirname $0)/package.sh ${version} | tail -n 1)
-echo "artifact = ${artifact}"
+artifact=$(git rev-parse --show-toplevel)/$($(dirname $0)/package.sh ${project_version} | tail -n 1)
+echo "artifact = [${artifact}]"
+
+if [ ! -e "${artifact}" ]
+then
+  echo "Packaging failed" >&2
+  exit 1
+fi
 
 timeout=60
-while [ -z "${public_ip_address}" -a ${timeout} -ne 0 ]
+while [[ -z "${public_ip_address}" && ${timeout} -ne 0 ]]
 do
   sleep 1
   ((--timeout))
@@ -21,7 +41,7 @@ do
   public_ip_address=$(aws ec2 describe-instances --instance-ids ${instance_ids} | jq '.Reservations[].Instances[].PublicIpAddress' | sed -e 's|"||g')
 done
 
-echo "public_ip_address = ${public_ip_address}"
+echo "public_ip_address = [${public_ip_address}]"
 
 key=~/.ssh/aws.pem
 user=ec2-user
@@ -29,7 +49,7 @@ while ! scp -o 'StrictHostKeyChecking no' -i ${key} "${artifact}" ${user}@${publ
 do
   sleep 1
 done
-ssh -o 'StrictHostKeyChecking no' -i ${key} ${user}@${public_ip_address} "cd / && gzip -cdq tmp/$(basename ${artifact}) | sudo tar xfpv -"
+ssh -o 'StrictHostKeyChecking no' -i ${key} ${user}@${public_ip_address} "cd / && sudo tar -x -f /tmp/${project_name}-${project_version}.tar.gz -p -v -z"
 
 aws ec2 stop-instances --instance-ids ${instance_ids}
 
--- scripts/package.sh
+++ scripts/package.sh
@@ -1,24 +1,42 @@
-#!/bin/bash
+#!/bin/bash -evx
 
-project_name=setter-for-catan
-
-version=$1
-if [ -z "${version}" ]
+if [[ "$(id -u)" != '0' || -z "$(id -u ec2-user 2>/dev/null)" || -z "${PROJECT_NAME}" || -z "${PROJECT_VERSION}" ]]
 then
-  version=$(git rev-parse HEAD)
+  project_name=$(basename $(git remote get-url origin) .git)
+  project_version=$1
+  if [ -z "${project_version}" ]
+  then
+    project_version=$(git rev-parse HEAD)
+  fi
+
+  docker run \
+      -i \
+      -v $(git rev-parse --show-toplevel):/home/ec2-user/${project_name} \
+      -e PROJECT_NAME=${project_name} \
+      -e PROJECT_VERSION=${project_version} \
+      packager.node \
+      /bin/bash -c "/home/ec2-user/${project_name}/scripts/package.sh"
+else
+  project_dir="/home/ec2-user/${PROJECT_NAME}"
+  package_dir="${project_dir}/package"
+  
+  rm -rf ${package_dir} &&
+      mkdir -p ${package_dir}
+  
+  mkdir -p ${package_dir}/root/{home,opt}
+
+  cp -r ${project_dir}/root/* ${package_dir}/root
+  cp -r ${project_dir}/build ${package_dir}/root/opt/${PROJECT_NAME}
+  
+  chown -R root:root ${package_dir}/root/*
+  chmod -R o+r ${package_dir}/root
+  find ${package_dir}/root -type d | xargs chmod o+x
+  
+  mkdir -p ${package_dir}/root/home/ec2-user/.nvm
+  npm install --prefix=${package_dir}/root/home/ec2-user/.nvm/versions/node/v10.15.3/ -g serve
+  
+  chown -R ec2-user:ec2-user ${package_dir}/root/home/ec2-user
+  
+  cd ${package_dir}/root && tar -c -f ${PROJECT_NAME}-${PROJECT_VERSION}.tar.gz -p -v -z *
+  echo "${package_dir}/root/${PROJECT_NAME}-${PROJECT_VERSION}.tar.gz" | sed -e "s|${project_dir}||"
 fi
-
-build_dir="$(git rev-parse --show-toplevel)/tmp"
-
-mkdir -p ${build_dir}
-cd ${build_dir}
-
-mkdir -p root/home/ec2-user/.nvm
-npm install --prefix=$(pwd)/root/home/ec2-user/.nvm/versions/node/v10.15.3/ -g serve
-
-mkdir -p root/opt
-cp -pr ${build_dir}/../build root/opt/${project_name}
-cp -pr ${build_dir}/../root/* root
-
-cd root && tar cfpv - * | gzip -f - > ../${project_name}-${version}.tar.gz
-echo "${build_dir}/${project_name}-${version}.tar.gz"
