6ba96a09cec2abd40e2b445808bff05df3ae5c87
Noel Yap <noel.yap+github.com@gmail.com>
2020-06-23 17:24:41 -0700

    Snapshot: Auth works! Profile not getting name :-( Breakpoints not working:   Source maps not generating

---
+++ src/mui.ts
@@ -0,0 +1,12 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export default {
+  theme: createMuiTheme({
+    palette: {
+      type: 'dark',
+    },
+    typography: {
+      useNextVariants: true
+    }
+  })
+}
